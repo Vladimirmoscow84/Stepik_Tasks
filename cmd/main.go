934f888fd6ca35f7057d877ea4f047ef46732f4a
Vladimirmoscow84 <ccr1@yandex.ru>
2025-05-19 16:34:33 +0300

    task 82P26 done

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,11 @@
 package main
 
-import (
-	task82Pt23 "Stepik_tasks/internal/slices"
-)
+import task82Pt26 "Stepik_tasks/internal/slices/task_8-2Pt2-6"
 
 func main() {
 
 	//task941.Task941()
 	//task942.Task942()
-	task82Pt23.Task82Pt23()
+	//task82Pt23.Task82Pt23()
+	task82Pt26.Task82Pt26()
 }
